b9dae56287f02b569ffcc4d2067fd74c0619d3b6
eaabad <8760053+egabad@users.noreply.github.com>
2023-09-06 10:40:51 +0800

    feat: verify that name is in the list

--- server/index.js
+++ server/index.js
@@ -6,21 +6,20 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// Hardcoded merkle root representing the whole nice list
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  // Grab the parameters from the front-end
+  const { proof, name } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  // Verify that name is in the list 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
-    res.send("You got a toy robot!");
+    res.send('You got a toy robot!');
   }
   else {
-    res.send("You are not on the list :(");
+    res.send('You are not on the list :(');
   }
 });
 
